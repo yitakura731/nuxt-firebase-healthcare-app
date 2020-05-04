bc40199146464f8ee544785af24ad3b54341d4ef
yitakura731 <yitakura731@gmail.com>
2020-05-04 17:11:21 +0900

    Modified to update running/meal calorie data to chart immediately

--- components/hc-calorie-chart.vue
+++ components/hc-calorie-chart.vue
@@ -2,62 +2,75 @@
 import { Bar } from 'vue-chartjs';
 
 export default {
-  name: 'HCRunningChart',
-  extends: Bar,
+  mixins: [Bar],
   props: {
     data: {
       type: Array,
       required: true
     }
   },
-  mounted() {
-    let dataMap = [];
-    const now = this.$moment();
-    const keyDay = now.subtract(now.isoWeekday() - 1, 'd');
-    for (let index = 0; index < 6; index++) {
-      dataMap.push({
-        key: this.$moment(keyDay),
-        value: 0
-      });
-      keyDay.subtract(7, 'd');
+  watch: {
+    data(newVal, oldVal) {
+      this.render();
     }
-    this.data.forEach(elem => {
-      const runDate = this.$moment(elem.date);
-      for (let index = 0; index < dataMap.length; index++) {
-        if (runDate.isAfter(dataMap[index].key)) {
-          dataMap[index].value += elem.calorie;
-          break;
-        }
+  },
+  mounted() {
+    this.render();
+  },
+  methods: {
+    render() {
+      let dataMap = [];
+      const now = this.$moment();
+      now.set('millisecond', 0);
+      now.set('second', 0);
+      now.set('minute', 0);
+      now.set('hour', 0);
+      const keyDay = now.subtract(now.isoWeekday() - 1, 'd');
+      for (let index = 0; index < 6; index++) {
+        dataMap.push({
+          key: this.$moment(keyDay),
+          value: 0
+        });
+        keyDay.subtract(7, 'd');
       }
-    });
-    dataMap = dataMap.reverse();
-    const chartData = {
-      labels: dataMap.map(elem => elem.key.format('YYYY / MM / DD')),
-      datasets: [
-        {
-          label: 'Bar Dataset',
-          data: dataMap.map(elem => elem.value),
-          backgroundColor: 'rgba(75,192,192,0.4)'
+      this.data.forEach(elem => {
+        const runDate = this.$moment(elem.date);
+        for (let index = 0; index < dataMap.length; index++) {
+          if (runDate.isAfter(dataMap[index].key)) {
+            dataMap[index].value += elem.calorie;
+            break;
+          }
         }
-      ]
-    };
-    const options = {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [
+      });
+      dataMap = dataMap.reverse();
+      const chartData = {
+        labels: dataMap.map(elem => elem.key.format('YYYY / MM / DD')),
+        datasets: [
           {
-            ticks: {
-              beginAtZero: true,
-              min: 0,
-              stepSize: 100
-            }
+            label: 'Bar Dataset',
+            data: dataMap.map(elem => elem.value),
+            backgroundColor: 'rgba(75,192,192,0.4)'
           }
         ]
-      }
-    };
-    this.renderChart(chartData, options);
+      };
+      const options = {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                min: 0,
+                stepSize: 100
+              }
+            }
+          ]
+        }
+      };
+      this.renderChart(chartData, options);
+    }
   }
 };
 </script>
