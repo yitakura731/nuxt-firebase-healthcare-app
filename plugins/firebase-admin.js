e062795152e2b0e88721972da92acb3ca5298cf4
yitakura731 <yitakura731@gmail.com>
2020-05-10 10:49:34 +0900

    Modified to set firebase admin sdk cert from environment

--- plugins/firebase-admin.js
+++ plugins/firebase-admin.js
@@ -3,10 +3,21 @@ let admin = null;
 if (process.server) {
   admin = require('firebase-admin');
   if (!admin.apps.length) {
-    const serviceAccount = require('../firebase-adminsdk.json');
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: process.env.FIREBASE_DATABASE_URL
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+
+        privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+        clientId: process.env.FIREBASE_CLIENT_ID,
+        authUri: process.env.FIREBASE_AUTH_URI,
+        tokenUri: process.env.FIREBASE_TOKEN_URI,
+        authProviderX509CertUrl:
+          process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        clientX509CertUrl: process.env.FIREBASE_CLIENT_EMAIL
+      }),
+      databaseURL: process.env.FIREBASE_CLIENT_X509_CERT_URL
     });
   }
 }
